8d33c23150951637d4efcc45269eb9e82a2c07f3
Naoto Yamaguchi <i33399_YAMAGUCHI@aisin-aw.co.jp>
2019-03-21 23:30:21 +0900

    Update bsp version to 3.15

--- docs/setup/setup_m3.sh
+++ docs/setup/setup_m3.sh
@@ -2,8 +2,8 @@ export WORK=`pwd`
 source ~/.config/user-dirs.dirs
 mkdir -p $WORK/proprietary/
 export PKGS_DIR=$WORK/proprietary/
-cp -f ${XDG_DOWNLOAD_DIR}/R-Car_Gen3_Series_Evaluation_Software_Package_for_Linux-20180627.zip $PKGS_DIR
-cp -f ${XDG_DOWNLOAD_DIR}/R-Car_Gen3_Series_Evaluation_Software_Package_of_Linux_Drivers-20180627.zip $PKGS_DIR
+cp -f ${XDG_DOWNLOAD_DIR}/R-Car_Gen3_Series_Evaluation_Software_Package_for_Linux-20181225.zip $PKGS_DIR
+cp -f ${XDG_DOWNLOAD_DIR}/R-Car_Gen3_Series_Evaluation_Software_Package_of_Linux_Drivers-20181225.zip $PKGS_DIR
 cd $WORK/meta-renesas
 sh meta-rcar-gen3/docs/sample/copyscript/copy_evaproprietary_softwares.sh -f $PKGS_DIR
 unset PKGS_DIR
@@ -12,6 +12,6 @@ source poky/oe-init-build-env
 cp $WORK/meta-experimental-own/docs/sample/conf/m3ulcb/poky-gcc/mmp/* $WORK/build/conf/
 
 # work around for error
-mv $WORK/meta-renesas/meta-rcar-gen3/recipes-core/ncurses/ncurses_6.0+20170715.bbappend $WORK/meta-renesas/meta-rcar-gen3/recipes-core/ncurses/ncurses_%.bbappend
+#mv $WORK/meta-renesas/meta-rcar-gen3/recipes-core/ncurses/ncurses_6.0+20170715.bbappend $WORK/meta-renesas/meta-rcar-gen3/recipes-core/ncurses/ncurses_%.bbappend
 
 unset WORK
